0055179e9c8d17fc45d1c47edd3640b663b97843
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-11-04 10:21:31 -0700

    Initial Commit

---
+++ CollectionViewMiniLesson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewMiniLesson
+//
+//  Created by Jordan Nelson on 11/4/15.
+//  Copyright © 2015 Jordan Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
